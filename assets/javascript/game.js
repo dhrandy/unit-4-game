9ea433040a4e422e7e16c86331b54efbc8905eae
dhrandy <dhrandy@gmail.com>
2018-04-03 06:31:19 -0400

    updated js file

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,6 +1,11 @@
-
 var win = 0;
 var losses = 0;
+var clickedCyrstal1 = 0;
+var clickedCyrstal2 = 0;
+var clickedCyrstal3 = 0;
+var clickedCyrstal4 = 0;
+var crystalScore = [];
+var sum = 0;
 var totalScore = 0;
 var crystal1 = Math.floor(Math.random() * 12) + 1;
 var crystal2 = Math.floor(Math.random() * 12) + 1;
@@ -17,26 +22,95 @@ var htmlComputerCrystal = //writes computerCrystal to html
         crystal3 = Math.floor(Math.random() * 12) + 1;
         crystal4 = Math.floor(Math.random() * 12) + 1;
     }
-    //while (crystal2 == crystal1 || crystal2 == crystal3 || crystal2 == crystal4){
-    //    crystal2 = Math.floor(Math.random() * 12) + 1;
-    //}
-    //while (crystal3 == crystal1 || crystal3 == crystal2 || crystal3 == crystal4){
-    //    crystal3 = Math.floor(Math.random() * 12) + 1;
-    //}
-    //while (crystal4 == crystal1 || crystal4 == crystal2 || crystal4 == crystal3){
-    //    crystal4 = Math.floor(Math.random() * 12) + 1;
-    //}
+
+function crystalClick(crystal){
+    clickedCyrstal = crystal;
+    crystalScore.push(clickedCyrstal);
+    console.log(crystalScore);
+    };
+
+   
+   
+    $("#crystal1").click(function(){
+        crystalClick(crystal1);
+        sum = crystalScore.reduce(function(a, b) {
+            return a + b;
+          }); 
+    });
+    console.log(crystalScore);
+    $("#crystal2").click(function(){
+        crystalClick(crystal2);
+        console.log(sum);
+    });
+    $("#crystal3").click(function(){
+        crystalClick(crystal3);
+        console.log(sum);
+    });
+    $("#crystal4").click(function(){
+        crystalClick(crystal4);
+        console.log(sum);
+    });
+
+    console.log(crystalScore);
+
+    //sum = crystalScore.reduce(function(a, b) {
+     //   return a + b;
+    //  });
+   
+   
 
 console.log(crystal1);
 console.log(crystal2);
 console.log(crystal3);
 console.log(crystal4);
+/* $("#crystal2").click(function(){
+    clickedCyrstal2 = crystal2;
+    console.log(clickedCyrstal2);
+    crystalScore.push(clickedCyrstal2);
+    console.log(crystalScore);
+    var sum = crystalScore.reduce(function(a, b) {
+        return a + b;
+    });
+      console.log(sum);
+});
+$("#crystal3").click(function(){
+    clickedCyrstal3 = crystal3;
+    console.log(clickedCyrstal3);
+    crystalScore.push(clickedCyrstal3);
+    console.log(crystalScore);
+    var sum = crystalScore.reduce(function(a, b) {
+        return a + b;
+      });
+      console.log(sum);
+});
+$("#crystal4").click(function(){
+    clickedCyrstal4 = crystal4;
+    console.log(clickedCyrstal4);
+    crystalScore.push(clickedCyrstal4);
+    console.log(crystalScore);
+    var sum = crystalScore.reduce(function(a, b) {
+        return a + b;
+      });
+      console.log(sum);
+});
+
+console.log(b);
+*/
+//for (crystalScore i = 0, sum = 0; i < crystalScore.length; sum += crystalScore[i++]);
+//console.log(sum);
+
+
+console.log(crystalScore);
+console.log(sum);
+//console.log(crystalScoreTotal);
+
+
+//var crystalScore = clickedCyrstal1 + clickedCyrstal2 + clickedCyrstal3 + clickedCyrstal4;
+//console.log(crystalScore);
 
 //var html =
 //"<p>Wins: " + wins + "</p>" +
 //"<br><p>Losses: " + losses + "</p>" +
-
 // Set the inner HTML contents of the #game div to our html string
 //document.querySelector("#gameScore").innerHTML = html;
 //}
-
---
+++ assets/javascript/test.js
@@ -0,0 +1,6 @@
+function go(name, age) {
+    var name = 2;
+    var age = 3;
+}
+
+var total = go(name + age)
