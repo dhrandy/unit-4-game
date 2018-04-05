bec68544afa0edb4c0b93a3d531956ad8a8ceef1
dhrandy <dhrandy@gmail.com>
2018-04-04 21:09:03 -0400

    cleaned code and updated some styling

--- assets/javascript/game.js
+++ assets/javascript/game.js
@@ -1,89 +1,87 @@
+//writing all the variables needed
 var win = 0;
 var losses = 0;
-var clickedCyrstal1 = 0;
-var clickedCyrstal2 = 0;
-var clickedCyrstal3 = 0;
-var clickedCyrstal4 = 0;
-var crystalScore = [];
+var gentlemanScore = [];
 var sum = 0;
-var totalScore = 0;
-var crystal1 = randomNumber();
-var crystal2 = randomNumber();
-var crystal3 = randomNumber();
-var crystal4 = randomNumber();
-var computerCrystal = Math.floor(Math.random() * 120) + 19;
-var htmlComputerCrystal = //writes computerCrystal to html
-    "<p>Enemy's Number # " + computerCrystal + "</p>";
-document.querySelector("#computerCrystal").innerHTML = htmlComputerCrystal;
+var gentleman1 = randomNumber();
+var gentleman2 = randomNumber();
+var gentleman3 = randomNumber();
+var gentleman4 = randomNumber();
+var computerHyde = Math.floor(Math.random() * 120) + 19;
+var htmlComputerHyde = //writes computerHyde to html
+    "<p>ENEMY'S NUMBER # " + computerHyde + "</p>";
+document.querySelector("#computerHyde").innerHTML = htmlComputerHyde;
 
+//used for the gentleman 1,2,3,4 variale to create the random number, didn't realy need this step, but wanted to try it.
 function randomNumber() {
     Math.floor(Math.random() * 12) + 1;
 }
 
-while (crystal1 == crystal2 || crystal1 == crystal3 || crystal1 == crystal4 || crystal2 == crystal1 || crystal2 == crystal3 || crystal2 == crystal4 || crystal3 == crystal1 || crystal3 == crystal2 || crystal3 == crystal4 || crystal4 == crystal1 || crystal4 == crystal2 || crystal4 == crystal3) {
-    crystal1 = Math.floor(Math.random() * 12) + 1;
-    crystal2 = Math.floor(Math.random() * 12) + 1;
-    crystal3 = Math.floor(Math.random() * 12) + 1;
-    crystal4 = Math.floor(Math.random() * 12) + 1;
+//the while loop makes sure that no 2 numbers match for the player
+while (gentleman1 == gentleman2 || gentleman1 == gentleman3 || gentleman1 == gentleman4 || gentleman2 == gentleman1 || gentleman2 == gentleman3 || gentleman2 == gentleman4 || gentleman3 == gentleman1 || gentleman3 == gentleman2 || gentleman3 == gentleman4 || gentleman4 == gentleman1 || gentleman4 == gentleman2 || gentleman4 == gentleman3) {
+    gentleman1 = Math.floor(Math.random() * 12) + 1;
+    gentleman2 = Math.floor(Math.random() * 12) + 1;
+    gentleman3 = Math.floor(Math.random() * 12) + 1;
+    gentleman4 = Math.floor(Math.random() * 12) + 1;
 }
 
-var htmlScore = //writes Score to html
+//Shows the initial win and loss score, which is 0 to 0.
+var htmlScore = 
     "<p>Wins: " + win + "</p>" +
     "<p>Losses: " + losses + "</p>";
 
 document.querySelector("#score").innerHTML = htmlScore;
 
-function crystalClick(crystal) {
-    clickedCyrstal = crystal;
-    crystalScore.push(clickedCyrstal);
-    sum = crystalScore.reduce(function(a, b) {
+//this function runs when you click on a gentleman
+function gentlemanClick(gentleman) {
+    gentlemanScore.push(gentleman); //pushes the selected random number from gentleman to variable
+    sum = gentlemanScore.reduce(function(a, b) { //this sums the variable
         return a + b;
     });
-    var htmlsum = //writes computerCrystal to html
+    var htmlsum = //writes computerHyde to html
         "<p>Gentlemens Sum = " + sum + "</p>";
     document.querySelector("#sum").innerHTML = htmlsum;
-    if (sum === computerCrystal) {
+    
+    //this runs if the sum and gentleman is equal
+    if (sum === computerHyde) {
         win++;
-        crystalScore = [0];
-        computerCrystal = Math.floor(Math.random() * 120) + 19;
-        var htmlComputerCrystal = //writes computerCrystal to html
-            "<p>Enemy's Number # " + computerCrystal + "</p>";
-        document.querySelector("#computerCrystal").innerHTML = htmlComputerCrystal;
-        crystal1 = Math.floor(Math.random() * 12) + 1;
-        crystal2 = Math.floor(Math.random() * 12) + 1;
-        crystal3 = Math.floor(Math.random() * 12) + 1;
-        crystal4 = Math.floor(Math.random() * 12) + 1;
+        var htmlComputerHyde = //writes computerHyde to html
+            "<p>Enemy's Number # " + computerHyde + "</p>";
+        document.querySelector("#computerHyde").innerHTML = htmlComputerHyde;
+        nextGame ();
 
-    } else if (sum > computerCrystal) {
-        losses++;
-        crystalScore = [0];
-        computerCrystal = Math.floor(Math.random() * 120) + 19;
-        var htmlComputerCrystal = //writes computerCrystal to html
-            "<p>Enemy's Number # " + computerCrystal + "</p>";
-        document.querySelector("#computerCrystal").innerHTML = htmlComputerCrystal;
-        crystal1 = Math.floor(Math.random() * 12) + 1;
-        crystal2 = Math.floor(Math.random() * 12) + 1;
-        crystal3 = Math.floor(Math.random() * 12) + 1;
-        crystal4 = Math.floor(Math.random() * 12) + 1;
+    } else if (sum > computerHyde) {
+        losses++; // add a loss if user goes over computer random number
+        nextGame ();
     }
-    var htmlScore = //writes Score to html
+
+    function nextGame () {
+        gentleman1 = Math.floor(Math.random() * 12) + 1;
+        gentleman2 = Math.floor(Math.random() * 12) + 1;
+        gentleman3 = Math.floor(Math.random() * 12) + 1;
+        gentleman4 = Math.floor(Math.random() * 12) + 1;
+        gentlemanScore = [0]; 
+        computerHyde = Math.floor(Math.random() * 120) + 19;
+    }
+
+    htmlScore = //writes Score to html
         "<p>Wins: " + win + "</p>" +
         "<p>Losses: " + losses + "</p>";
 
     document.querySelector("#score").innerHTML = htmlScore;
 };
-$("#crystal1").click(function() {
-    crystalClick(crystal1);
+$("#gentleman1").click(function() {
+    gentlemanClick(gentleman1);
 });
 
-$("#crystal2").click(function() {
-    crystalClick(crystal2);
+$("#gentleman2").click(function() {
+    gentlemanClick(gentleman2);
 });
 
-$("#crystal3").click(function() {
-    crystalClick(crystal3);
+$("#gentleman3").click(function() {
+    gentlemanClick(gentleman3);
 });
 
-$("#crystal4").click(function() {
-    crystalClick(crystal4);
+$("#gentleman4").click(function() {
+    gentlemanClick(gentleman4);
 });
