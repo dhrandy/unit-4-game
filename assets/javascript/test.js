5e3ec8fae5a3bc1733b4c0b4afa5e43785838c6d
dhrandy <dhrandy@gmail.com>
2018-04-07 17:34:49 -0400

    updated gentleman to enlarge onhover in html

--- assets/javascript/test.js
+++
@@ -1,6 +0,0 @@
-function go(name, age) {
-    var name = 2;
-    var age = 3;
-}
-
-var total = go(name + age)
---
+++ assets/javascript/zoom.js
@@ -0,0 +1,35 @@
+$(document).ready(function(){
+    $('#gentleman1').hover(function() {
+        $("#gentleman1").addClass('transition');
+    
+    }, function() {
+        $("#gentleman1").removeClass('transition');
+    });
+});
+
+$(document).ready(function(){
+    $('#gentleman2').hover(function() {
+        $("#gentleman2").addClass('transition');
+    
+    }, function() {
+        $("#gentleman2").removeClass('transition');
+    });
+});
+
+$(document).ready(function(){
+    $('#gentleman3').hover(function() {
+        $("#gentleman3").addClass('transition');
+    
+    }, function() {
+        $("#gentleman3").removeClass('transition');
+    });
+});
+
+$(document).ready(function(){
+    $('#gentleman4').hover(function() {
+        $("#gentleman4").addClass('transition');
+    
+    }, function() {
+        $("#gentleman4").removeClass('transition');
+    });
+});
